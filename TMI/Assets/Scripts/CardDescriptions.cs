38c3325bac673fc906277b2d024b4daaf34d7b88
Adrian4201 <cookar6@miamioh.edu>
2025-09-04 17:49:35 -0400

    push

---
+++ TMI/Assets/Scripts/CardDescriptions.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CardDescriptions : MonoBehaviour
+{
+    [SerializeField] private TMP_Text Title;
+
+    [SerializeField] private TMP_Text mana;
+    
+    [SerializeField] private TMP_Text Description;
+}
