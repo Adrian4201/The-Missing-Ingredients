f2aac423357438abbb0698b39e1bcf37f98e5d56
Adrian4201 <cookar6@miamioh.edu>
2025-09-09 15:17:56 -0400

    push

---
+++ TMI/Assets/Scripts/CardDetails/Handdetails.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class HAnd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private SplineContainer spline;
+
+    private readonly List<CardDescriptions> cards = new();
+
+    public IEnumerator AddCard(CardDescriptions,)
+}
