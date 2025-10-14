47f0ffe51a5a54ae39e30fa3c389d5dd688e1a6f
Adrian4201 <cookar6@miamioh.edu>
2025-10-13 21:13:35 -0400

    Turn system

--- TMI/Assets/Scripts/System/EnemySystem.cs
+++ TMI/Assets/Scripts/System/EnemySystem.cs
@@ -22,8 +22,26 @@ public class EnemySystem : Singleton<EnemySystem>
     }
     private IEnumerator EnremyTurnperformer(Enemyturn enemyturn)
     {
-        Debug.Log("enemyturn");
-        yield return new WaitForSeconds(2f);
-        Debug.Log("turn done");
+        DrawCard enemydraw = new DrawCard(1);
+        ActionSystem.Instance.Preform(enemydraw);
+        yield return new WaitUntil(() => !ActionSystem.Instance.Isperforming);
+
+        if (CardSystem.Instance.Hand.Count > 0)
+        {
+            Cards card = CardSystem.Instance.Hand[0];
+            Debug.Log("Enemy picked up card: " + card.Title);
+
+            // Perform enemy attack
+            EnemyAttack enemAttack = new EnemyAttack(card);
+            ActionSystem.Instance.AddAction(enemAttack);
+            ActionSystem.Instance.Preform(enemAttack);
+            yield return new WaitUntil(() => !ActionSystem.Instance.Isperforming);
+        }
+        else
+        {
+            Debug.LogError("No cards for enemy!");
+        }
+
+        Debug.Log("Enemy turn completed");
     }
 }
